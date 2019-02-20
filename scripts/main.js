1d6569186c127023f0ea1dc70dfcd777b514f70c
ribo <roibeard.mac.unfraidh@gmail.com>
2019-02-20 10:27:41 +0100

    Remove old css and js content. Reorganize html. Include bootstrap.

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+  beginApp = () => {
+
+    $('.splash').fadeTo("slow", 0.01);
+    $('.menu').fadeTo("slow", 1);
+
+  }
+  setTimeout(beginApp, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
