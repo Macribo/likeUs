e42f9242ec9fd5a7dc23810dbaae122e95abd992
ribo <roibeard.mac.unfraidh@gmail.com>
2019-02-23 20:40:56 +0100

    Start rebuild.

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,36 @@
 $(document).ready(function () {
 
-  beginApp = () => {
 
-    $('.splash').fadeTo("slow", 0.01);
-    $('.menu').fadeTo("slow", 1);
+  setTimeout(function () {
+    $('.splash').fadeTo("fast", 0.01);
+    $('.menu').fadeTo("fast", 1);
+
+  }, 1000);
+
+
+
+
+  run1 = () => {
+
+    setTimeout(function () {
+      $('.main-menu').fadeOut();
+      setTimeout(function () {
+        $('.select-pig').fadeTo("fast", 1);
+
+      }, 1000);
+
+    }, 1000);
+
 
   }
-  setTimeout(beginApp, 1000);
 
+  $('.run').on('click', run1);
+
+  $('.ok-slaughter').on('click', function () {
+    $('.jumbotron').fadeOut();
+    $('.pigs').fadeTo("slow", 1);
+
+  });
 
 
 
