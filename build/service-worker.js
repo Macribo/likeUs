d80ffb9c39a1c724b83d6a6a33e075cdd9d4d32d
ribo <roibeard.mac.unfraidh@gmail.com>
2019-03-17 19:49:43 +0100

    add predeploy to package.json

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa29f97452c6f6f56945c5b53e8fda60.js"
+  "/Macribo/likeUs/precache-manifest.463f9f5274957af83997000526226d39.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Macribo/likeUs/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
