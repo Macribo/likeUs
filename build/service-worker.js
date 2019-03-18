2ed7daca9e76badc45771ff4194fe451dcf449f6
ribo <roibeard.mac.unfraidh@gmail.com>
2019-03-18 22:20:20 +0100

    Replace package.json.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Macribo/likeUs/precache-manifest.463f9f5274957af83997000526226d39.js"
+  "/likeUs/precache-manifest.b734c5b466d30d3793c830c5a836ed16.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Macribo/likeUs/index.html", {
+workbox.routing.registerNavigationRoute("/likeUs/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
