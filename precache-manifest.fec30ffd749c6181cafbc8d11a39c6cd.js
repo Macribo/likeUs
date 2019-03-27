db4689d895904f3a6a3bb6fcec138b30b31a6872
ribo <roibeard.mac.unfraidh@gmail.com>
2019-03-27 12:31:07 +0100

    Updates

--- precache-manifest.fec30ffd749c6181cafbc8d11a39c6cd.js
+++ precache-manifest.fec30ffd749c6181cafbc8d11a39c6cd.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/likeUs/static/media/baton.b73b22ae.png"
   },
   {
-    "revision": "4f57ff4bd1e6192e3deb",
+    "revision": "66e6a738fb847d0fd26e",
     "url": "/likeUs/static/css/main.e70dcfd8.chunk.css"
   },
   {
@@ -12,20 +12,20 @@ self.__precacheManifest = [
     "url": "/likeUs/static/js/runtime~main.a3ed4e77.js"
   },
   {
-    "revision": "7b08235cffce4d245aa954baf924426f",
-    "url": "/likeUs/static/media/pig2.7b08235c.png"
+    "revision": "e40d307f4df8c91cab25d32202ca81b6",
+    "url": "/likeUs/static/media/pig2b.e40d307f.png"
   },
   {
-    "revision": "b47f323d5531d737b593",
-    "url": "/likeUs/static/js/2.26627a47.chunk.js"
+    "revision": "fdb8eb24ca44045bcb2f",
+    "url": "/likeUs/static/js/2.435f164b.chunk.js"
   },
   {
     "revision": "e146062421c53ebef22e49aba8856206",
     "url": "/likeUs/static/media/slaughter-mask02.e1460624.png"
   },
   {
-    "revision": "4f57ff4bd1e6192e3deb",
-    "url": "/likeUs/static/js/main.8c9270e0.chunk.js"
+    "revision": "66e6a738fb847d0fd26e",
+    "url": "/likeUs/static/js/main.92181e39.chunk.js"
   },
   {
     "revision": "0ea95b3f59c8386126dd7060d4be1a9e",
@@ -36,15 +36,15 @@ self.__precacheManifest = [
     "url": "/likeUs/static/media/door.cb0d15b9.jpg"
   },
   {
-    "revision": "e40d307f4df8c91cab25d32202ca81b6",
-    "url": "/likeUs/static/media/pig2b.e40d307f.png"
+    "revision": "7b08235cffce4d245aa954baf924426f",
+    "url": "/likeUs/static/media/pig2.7b08235c.png"
   },
   {
-    "revision": "b47f323d5531d737b593",
+    "revision": "fdb8eb24ca44045bcb2f",
     "url": "/likeUs/static/css/2.12bfc508.chunk.css"
   },
   {
-    "revision": "ea67c7a1cb0b3da45475c2cff8d23a96",
+    "revision": "256ec7135bdc38370c716894e24777c3",
     "url": "/likeUs/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/likeUs/precache-manifest.9644af05db5ce0796674b7e6636f36e8.js"
+  "/likeUs/precache-manifest.fec30ffd749c6181cafbc8d11a39c6cd.js"
 );
 
 workbox.clientsClaim();
